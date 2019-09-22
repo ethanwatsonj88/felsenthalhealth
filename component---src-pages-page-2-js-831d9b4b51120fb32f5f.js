a51ab5a497784f40a7656aeeca66bfa7a2eec0c6
ethanwatsonj88 <ethanwatsonj88@gmail.com>
2019-09-22 14:23:34 -0400

    fff

---
+++ component---src-pages-page-2-js-831d9b4b51120fb32f5f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[47],{284:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(162),c=a(234),r=a(233);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-831d9b4b51120fb32f5f.js.map
---
+++ styles-f8287227913a7380f7a9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{172:function(n,o,w){},225:function(n,o,w){},226:function(n,o,w){}}]);
+//# sourceMappingURL=styles-f8287227913a7380f7a9.js.map
